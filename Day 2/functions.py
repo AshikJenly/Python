95d983b08cb02d1d73c34ddc0b3f38505ffc8445
Anu <135311321+anujenly@users.noreply.github.com>
2023-06-03 23:02:45 +0530

    AddedDay 2

---
+++ Day 2/functions.py
@@ -0,0 +1,7 @@
+def p(n):
+    for i in range(n):
+        x=n-i
+        print(x*' ',end='')
+        print(i*' *')
+
+p(100)
---
+++ Day 2/variables.py
@@ -0,0 +1,14 @@
+x=8
+print(x)
+y=2
+print(x+y)
+name="youtube"
+print(name)
+print (name+"rocks")
+print(name[0])
+print(name[-1])
+print(name[0:2])
+print(name[1:4])
+print('my'+name[3:])
+myname='anu'
+len(myname)
